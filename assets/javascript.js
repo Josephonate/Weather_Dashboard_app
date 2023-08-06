a4fe87ff870c2f74090960db06bd5326376864ea
Josephonate <131502920+Josephonate@users.noreply.github.com>
2023-08-06 14:10:40 -0400

    got save button to work with user input

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,13 +1,22 @@
-var weather = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={83689c9844b7838e59ff072878e7fe48}";
-// var apiKey ="83689c9844b7838e59ff072878e7fe48";
-fetch(weather)
-.then(function(repsonse){
-    return repsonse;
-})
-.then(function(data){
-    console.log(data);
-})
+var saveButton = $('#search');
 
-function saveCity(){
 
-}
+    saveButton.click(function () {
+        var city = $('#cityInput').val();
+        var weatherAPI = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=83689c9844b7838e59ff072878e7fe48';
+        console.log(city);
+
+        fetch(weatherAPI)
+            .then(function (repsonse) {
+                return repsonse.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            })
+            .catch(function (error) {
+                console.error('Error fetching weather data: ', error);
+            });
+
+    })
+
+
