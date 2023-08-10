d677d290763b9914bc32cc79b6f466813c66a5c4
Josephonate <131502920+Josephonate@users.noreply.github.com>
2023-08-09 22:13:18 -0400

    worked with tutor to make changes and show examples of retriving data

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,27 +1,65 @@
 var saveButton = $('#search');
-var city = $('.city')
-var temp = $('.temp')
-var weatherDescription = $('.weatherDescription')
-var feelsLike = $('.feelsLike')
-var humidity = $('.humidity')
-var windspeed = $('.windspeed')
-
-    saveButton.click(function () {
-        var city = $('#cityInput').val();
-        var weatherAPI = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=83689c9844b7838e59ff072878e7fe48';
-        console.log(city);
-
-        fetch(weatherAPI)
-            .then(function (repsonse) {
-                return repsonse.json();
-            })
-            .then(function (data) {
-                console.log(data);
-
-            })
-            .catch(function (error) {
-                console.error('Error fetching weather data: ', error);
-            });
-    })
 
 
+saveButton.click(function () {
+    var city = $('#cityInput').val();
+    var weatherAPI = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=83689c9844b7838e59ff072878e7fe48';
+    console.log(city);
+
+    fetch(weatherAPI)
+        .then(function (repsonse) {
+            return repsonse.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            data.list[0].main.humidity
+
+        })
+        .catch(function (error) {
+            console.error('Error fetching weather data: ', error);
+        });
+})
+
+// function showPrimaryCar(model){
+//     $("#primaryCar").empty()
+//     const primary = cars.filter((car) => car.model === model)[0];
+
+//     const html = $(`
+   
+//    <div class="card col-12">
+//     <div class="card-body">
+//         <h5 class="card-title">${primary.name}</h5>
+//         <p>Model: ${primary.model}</p>
+//         <p>Make: ${primary.make}</p>
+//         <p>Year: ${primary.year}</p>
+//     </div>
+//     </div>   
+// `)
+
+//     $("#primaryCar").append(html);
+// }
+
+
+// function showOtherCars(){
+//     $("#otherCar").empty();
+//     for (let i = 0; i < cars.length; i++) {
+//         if(i % 2 === 0){
+//          const car = cars[i];
+
+//          const html = $(`
+//             <div class="col">
+//                 <div class="card">
+//                     <div class="card-body">
+//                         <h5 class="card-title">${car.name}</h5>
+//                         <p>Model: ${car.model}</p>
+//                         <p>Make: ${car.make}</p>
+//                         <p>Year: ${car.year}</p>
+//                     </div>
+//                 </div>   
+//             </div>
+//             `);
+
+//             $("#otherCars").append(html);
+//         }
+//     }
+// }
